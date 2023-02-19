9dcba303869af624dc25d409fe1ecf3085f4b529
Sabry Awad <dr.sabry1997@gmail.com>
2023-02-19 16:08:02 +0200

    perform multiple assertions on the output

--- tests/cli.rs
+++ tests/cli.rs
@@ -10,7 +10,10 @@ fn test_echo_with_newline() -> Result<(), Box<dyn std::error::Error>> {
     let expected_output = "Hello, world!\n";
     let mut cmd = Command::cargo_bin(PROGRAM_NAME)?;
     let assert = cmd.arg("Hello, world!").assert();
-    assert.success().stdout(predicate::eq(expected_output));
+    assert
+        .success()
+        .stdout(predicate::eq(expected_output))
+        .stdout(predicate::str::ends_with("\n"));
     Ok(())
 }
 
@@ -19,6 +22,9 @@ fn test_echo_without_newline() -> Result<(), Box<dyn std::error::Error>> {
     let expected_output = "Hello, world!";
     let mut cmd = Command::cargo_bin(PROGRAM_NAME)?;
     let assert = cmd.arg("-n").arg("Hello, world!").assert();
-    assert.success().stdout(predicate::eq(expected_output));
+    assert
+        .success()
+        .stdout(predicate::eq(expected_output))
+        .stdout(predicate::str::ends_with("\n").not());
     Ok(())
 }
