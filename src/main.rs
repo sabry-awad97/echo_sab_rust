2328e027d9491782a8b1777faa83524b9ee44236
Sabry Awad <dr.sabry1997@gmail.com>
2023-02-19 15:47:39 +0200

    Print text to the console

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io::Write;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "echo", about = "Print text to the console")]
+struct Options {
+    #[structopt(help = "The text to print")]
+    text: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let options = Options::from_args();
+    let output = format!("{}\n", options.text);
+
+    std::io::stdout()
+        .write_all(output.as_bytes())
+        .expect("Failed to write to stdout");
 }
